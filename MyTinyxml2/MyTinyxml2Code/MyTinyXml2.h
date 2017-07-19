82e87818ab4fad934d3fd0f381c05175a7a32cfa
leal <bocheng8025@gmail.com>
2017-07-19 19:56:34 +0800

    Add files via upload

---
+++ MyTinyxml2/MyTinyxml2Code/MyTinyXml2.h
@@ -0,0 +1,18 @@
+/*@{*/
+/************************************************************************/
+/**
+@file MyTinyxml2.h
+@brief xml½âÎöÄ£¿é
+@author lealcheng
+@data 2017-07-19
+@version 2.6.2
+*/
+/************************************************************************/
+#pragma once
+
+#define TIXML_USE_STL
+#include "tinystr.h"
+#include "tinyxml.h"
+/*
+* @}
+*/
